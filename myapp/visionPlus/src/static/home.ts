517c6bdd177ce151908f449e8fa643bb45b85382
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-10-18 11:05:03 +0800

    update

--- myapp/visionPlus/src/static/home.ts
+++ myapp/visionPlus/src/static/home.ts
@@ -1,7 +1,7 @@
 export const pipelineDemo = [];
 
 export const videoDemo = [
-  /*{
+  /* {
     name: '新人制作一个pipeline',
     img: '/static/assets/images/ad/video-cover2-thumb.png',
     url: 'https://docker-76009.sz.gfp.tencent-cloud.com/kubeflow/make_pipeline.mp4',
